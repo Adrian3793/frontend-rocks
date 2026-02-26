de868925df41873210b558579f197d4be0071228
Adrian3793 <105550726+Adrian3793@users.noreply.github.com>
2026-02-26 13:40:14 +0000

    changes

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? 'https://giovannigiordano.github.io/frontend-rocks/' : '/frontend-rocks',
+  base: process.env.NODE_ENV === 'production' ? 'https://github.com/Adrian3793/frontend-rocks' : '/frontend-rocks',
   plugins: [react(), tailwindcss()],
 })
-
